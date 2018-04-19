57c3f6e5d016270e452242a3ee00cbad3f18fdbd
Christoph Böhner-Figas <boehner@team23.de>
2018-04-19 16:31:35 +0200

    Prepare base viewlet

--- ade25/base/browser/cookieconsent.py
+++ ade25/base/browser/cookieconsent.py
@@ -2,7 +2,17 @@
 """Module providing cookie consent viewlet"""
 from plone.app.layout.viewlets import ViewletBase
 
+from plone import api
+
 
 class CookieConsentViewlet(ViewletBase):
     """ Context aware responsive navigation viewlet """
 
+    @staticmethod
+    def enabled():
+        enabled = api.portal.get_registry_record(
+            name='ade25.base.cc_enabled'
+        )
+        if enabled:
+            return enabled
+        return False
